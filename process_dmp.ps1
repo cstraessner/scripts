9e5ae76fd7ded9f49e87f219c3c9ca6cc8c57a25
cstraessner <christian823@hotmail.com>
2019-07-31 15:36:00 +0200

    changed  to function

--- process_dmp.ps1
+++ process_dmp.ps1
@@ -1,25 +1,25 @@
 <#  
 finds  all *.dmp file  under $basdir  ( recursive) and  runs !analyze  against  each
 out  put  goes  to same dir as  dump  as  dumpname.log
+usage:  scan-dumps -basedir  "c:\dumps"
 #>
 
 
-
-$basedir = "E:\mydumps\"
-
+function scan-dumps{ param( [string]$basedir)
 
 
 $dumplist = get-childitem -Recurse $basedir  -Include *.dmp
 
 
-
-
  
 foreach ($dump in $dumplist)
-{
-write-host "processing dump" $dump 
-$command = "kd.exe   -c '!analyze -v;q' -z $dump"
-$out = Invoke-Expression $command
-$out | out-file  -filepath "$dump.log" -Force
+	{
+	write-host "processing dump" $dump 
+
    
+	}
 }
+
+
+
+
